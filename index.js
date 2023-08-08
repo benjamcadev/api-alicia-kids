7993777f32930be5ee063aafb7626bef6f3654b2
Benjamin Cortes <benjamca@hotmail.com>
2023-08-08 16:47:01 -0400

    Modelando son sequelize

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("jelou");
+import app from './src/app.js'
+import { sequelize }  from './database/conexion.js' 
+
+const main = async () => {
+    try {
+        await sequelize.authenticate()
+        console.log('Connection has been established successfully.')
+    } catch (error) {
+        console.error('Unable to connect to the database:', error)
+        
+    }
+   
+}
+
+main()
+
+app.listen(3000)
+
+console.log('Servidor corriendo en el port', 3000);
