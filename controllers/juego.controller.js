3e5d291259639c7e0cf192ac50de754ea04e2578
Benjamin Cortes <benjamca@hotmail.com>
2023-10-11 20:13:03 -0300

    Agregando headers en juego.controller para permitir el envio correcto de los datos sin bloqueo por cors

--- controllers/juego.controller.js
+++ controllers/juego.controller.js
@@ -7,6 +7,13 @@ export const getJuegos = async(req,res) => {
             flag_delete_juego: false
         }
     })
+
+    res.set({
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials" : "true" 
+    });
+
     res.status(200).json(
         {
             status: 'success',
